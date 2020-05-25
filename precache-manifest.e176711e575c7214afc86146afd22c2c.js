db0606b432dfec8c9e3067f2025a168fa0eb983d
Daniel Bank <daniel.p.bank@gmail.com>
2020-05-24 21:59:05 -0700

    Updates

---
+++ precache-manifest.e176711e575c7214afc86146afd22c2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd2c43a526eadabf990",
+    "url": "/rust-wasm-react/b0d672ee7b6b9aa72504.module.wasm"
+  },
+  {
+    "revision": "695fbab8c8dc7bde8f6f99816dff5fac",
+    "url": "/rust-wasm-react/index.html"
+  },
+  {
+    "revision": "8b5169a31c179da0fa78",
+    "url": "/rust-wasm-react/static/css/main.d275bea4.chunk.css"
+  },
+  {
+    "revision": "141df75a4884e25c79a2",
+    "url": "/rust-wasm-react/static/js/2.811db75c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rust-wasm-react/static/js/2.811db75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd2c43a526eadabf990",
+    "url": "/rust-wasm-react/static/js/3.73b7a6ca.chunk.js"
+  },
+  {
+    "revision": "8b5169a31c179da0fa78",
+    "url": "/rust-wasm-react/static/js/main.4c2b86e2.chunk.js"
+  },
+  {
+    "revision": "ee634a6270d7e5f31468",
+    "url": "/rust-wasm-react/static/js/runtime-main.ed4cf56a.js"
+  }
+]);
